66a5e491f0baa428c3f87b066134110a50495e32
Muhammed souban <muhammedsoubankunnummel123@gmail.com>
2024-07-09 18:28:37 +0530

    fet: cron job

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,9 +2,18 @@ import express from 'express'
 import { tryCatch } from '../middlewares/tryCatch.js'
 import { login, SignUp } from '../controllers/auth/authController.js'
 
+import cron from 'node-cron'
+
 const Router = express.Router()
 
 Router.post('/api/signup', tryCatch(SignUp))
 Router.post("/api/login",tryCatch(login))
 
-export default Router
+
+cron.schedule('* * * * *', () => {
+    console.log('Running a task every minute');
+  });
+  
+
+
+export default Router    
